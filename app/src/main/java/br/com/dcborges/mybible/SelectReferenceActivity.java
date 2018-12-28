b477b65ba9ee631ee34aec7182ad2671ebf880c4
Davi Borges <davi.borges@wooba.com.br>
2018-12-28 17:57:30 -0200

    first commit

---
+++ app/src/main/java/br/com/dcborges/mybible/SelectReferenceActivity.java
@@ -0,0 +1,12 @@
+package br.com.dcborges.mybible;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SelectReferenceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_reference);
+    }
+}
